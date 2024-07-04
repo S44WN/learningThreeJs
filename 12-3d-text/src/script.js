f18d98ee14b5def3a741fd5f5f37411a597c5b28
Saawn <itissaavn@gmail.com>
2024-07-04 12:19:03 +0530

    Optimization: Refactor code to use shared material for 3D text and floating shapes

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -56,22 +56,19 @@ fontLoader.load(
       -(textGeometry.boundingBox.max.z - 0.03) * 0.5 // Subtract bevel thickness
     );
 
-    const textMaterial = new THREE.MeshMatcapMaterial({
+    const material = new THREE.MeshMatcapMaterial({
       matcap: matcapTexture,
     });
-    const text = new THREE.Mesh(textGeometry, textMaterial);
+    const text = new THREE.Mesh(textGeometry, material);
     scene.add(text);
 
     /* 
     floating shapes
     */
 
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
     for (let i = 0; i < 100; i++) {
-      const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
-      const donutMaterial = new THREE.MeshMatcapMaterial({
-        matcap: matcapTexture,
-      });
-      const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+      const donut = new THREE.Mesh(donutGeometry, material);
       donut.position.x = (Math.random() - 0.5) * 10;
       donut.position.y = (Math.random() - 0.5) * 10;
       donut.position.z = (Math.random() - 0.5) * 10;

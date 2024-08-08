58215215eaf0b8ec98f2f4b00c83a58c739b279a
Saawn <itissaavn@gmail.com>
2024-08-08 10:15:13 +0530

    refactor: Update OrbitControls import in Experience component

--- 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,15 +1,16 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  return (
+    <>
+      <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+      <color args={["#241a1a"]} attach="background" />
 
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
     </>
+  );
 }

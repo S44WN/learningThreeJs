463f5bd4f143b22a577c29320567c732b029841a
Saawn <itissaavn@gmail.com>
2024-08-09 18:41:18 +0530

    feat: Add laptop model and environment

--- 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,11 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, useGLTF, OrbitControls } from "@react-three/drei";
 
 export default function Experience() {
+  const computer = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
+
   return (
     <>
       <OrbitControls makeDefault />
 
+      <Environment preset="city" />
+
       <color args={["#241a1a"]} attach="background" />
+
+      <primitive object={computer.scene} />
 
       <mesh>
         <boxGeometry />

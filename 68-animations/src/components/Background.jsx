d576722b6971d88e170cd1ce10c03ecab0201996
Saawn <itissaavn@gmail.com>
2024-09-02 23:05:51 +0530

    feat: Add framer-motion and gsap for animations

--- 68-animations/src/components/Background.jsx
+++ 68-animations/src/components/Background.jsx
@@ -1,7 +1,39 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
+
+import { gsap } from "gsap";
+import { useFrame } from "@react-three/fiber";
+import { useScroll } from "@react-three/drei";
+
 export const Background = () => {
   const skyMaterial = useRef();
+  const tl = useRef(); //timeline
+  const skyData = useRef({
+    color: "#313131",
+  });
+
+  useEffect(() => {
+    tl.current = gsap.timeline();
+
+    tl.current
+      .to(skyData.current, {
+        duration: 1,
+        color: "#ffc544",
+      })
+      .to(skyData.current, {
+        duration: 1,
+        color: "#7c9e4f",
+      });
+  }, []);
+
+  const scrollData = useScroll();
+
+  useFrame(() => {
+    if (!tl.current) return;
+
+    tl.current.progress(scrollData.offset);
+    skyMaterial.current.color.set(skyData.current.color);
+  });
 
   return (
     <mesh rotation-x={Math.PI / 4}>
